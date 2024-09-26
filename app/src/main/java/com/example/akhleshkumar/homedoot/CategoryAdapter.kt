bc2bfeec81b0697551d600a0a30cafbe877156a9
Akhlesh kumar <akhleshpbt@gmail.com>
2024-09-26 14:48:09 +0530

    Created RecyclerView for category items

---
+++ app/src/main/java/com/example/akhleshkumar/homedoot/CategoryAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.akhleshkumar.homedoot
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryAdapter(private val context: Context, private val categoryList: List<Category>) :
+RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false)
+        return CategoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val category = categoryList[position]
+        holder.categoryName.text = category.name
+        holder.categoryIcon.setImageResource(category.iconResId)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryList.size
+    }
+
+    inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryIcon: ImageView = itemView.findViewById(R.id.category_icon)
+        val categoryName: TextView = itemView.findViewById(R.id.category_name)
+    }
+}
--- app/src/main/java/com/example/akhleshkumar/homedoot/HomeDootFragment.kt
+++ app/src/main/java/com/example/akhleshkumar/homedoot/HomeDootFragment.kt
@@ -6,17 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 
 class HomeDootFragment : Fragment() {
-  lateinit var list: ArrayList<Int>
+  private lateinit var list: ArrayList<Int>
     var sliderHandler: Handler = Handler()
 
     lateinit var tableLayout: DotsIndicator
     lateinit var viewPager: ViewPager2
     lateinit var sliderAdapter: SliderAdapter
+    lateinit var rvCategery: RecyclerView
+    lateinit var rvServices : RecyclerView
+    lateinit var serviceList :List<Category>
+    lateinit var  categoryList : List<Category>
+    lateinit var layoutManager : GridLayoutManager
+    lateinit var categoryAdapter: CategoryAdapter
+    lateinit var serviceAdapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +42,26 @@ class HomeDootFragment : Fragment() {
         list.add(R.drawable.house_keeping)
         list.add(R.drawable.kitchen)
         list.add(R.drawable.pest_control)
+
+        categoryList = listOf(Category("Cleaning Services",R.drawable.pest_control_vector),
+            Category("Pest Control Services",R.drawable.pest_control_vector),
+            Category("Cleaning Services",R.drawable.pest_control_vector),
+            Category("Cleaning Services",R.drawable.pest_control_vector),
+            Category("Cleaning Services",R.drawable.pest_control_vector),
+            Category("Cleaning Services",R.drawable.pest_control_vector))
+
+        serviceList = listOf(Category("Cleaning Services",R.drawable.pest_control),
+        Category("Pest Control Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+            Category("Cleaning Services",R.drawable.pest_control),
+        Category("Pest Control Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control),
+        Category("Cleaning Services",R.drawable.pest_control))
         return view
     }
 
@@ -40,7 +69,17 @@ class HomeDootFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
        viewPager = view.findViewById(R.id.viewPager)
         tableLayout = view.findViewById(R.id.tabLayout)
+        rvCategery = view.findViewById(R.id.rv_category)
+        rvServices = view.findViewById(R.id.rv_services)
 
+        layoutManager = GridLayoutManager(requireContext(),3)
+        categoryAdapter = CategoryAdapter(requireContext(), categoryList)
+        serviceAdapter = CategoryAdapter(requireContext(),serviceList)
+        rvCategery.layoutManager = layoutManager
+        rvCategery.adapter = categoryAdapter
+
+        rvServices.layoutManager = GridLayoutManager(requireContext(),3)
+        rvServices.adapter = serviceAdapter
 
         sliderAdapter = SliderAdapter( list)
         viewPager.setAdapter(sliderAdapter)
