cf6609ef6f67bce741ce4ad1799a2cfc56255904
User <sachinamz857@gmail.com>
2024-10-16 23:38:07 +0530

    payment activity

---
+++ app/src/main/java/com/example/akhleshkumar/homedoot/activities/PaymentMethodActivity.kt
@@ -0,0 +1,16 @@
+package com.example.akhleshkumar.homedoot.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.akhleshkumar.homedoot.R
+
+class PaymentMethodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_method)
+
+    }
+}
