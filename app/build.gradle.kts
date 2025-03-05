ee5021446634c4311838254554cd5759e15ad59d
akhlesh-gangwar <akhleshpbt@gmail.com>
2025-03-06 02:21:54 +0530

    no dark mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.akhleshkumar.homedoot"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
